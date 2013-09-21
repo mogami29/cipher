330ec48248a8e034cd4dac4a30ed91fc2edd24bd
最上嗣生 <mogami@saijou-no-MacBook-Air.local>
2013-09-21 19:20:12 +0900

    Initial Commit

---
+++ front/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  front
+//
+//  Created by 最上嗣生 on 2013/09/21.
+//  Copyright (c) 2013年 最上嗣生. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ frontTests/frontTests.m
@@ -0,0 +1,34 @@
+//
+//  frontTests.m
+//  frontTests
+//
+//  Created by 最上嗣生 on 2013/09/21.
+//  Copyright (c) 2013年 最上嗣生. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface frontTests : XCTestCase
+
+@end
+
+@implementation frontTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
