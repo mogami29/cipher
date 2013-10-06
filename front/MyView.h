298d8e129469bcdd94aa4f7a54cb91f50ca12079
最上嗣生 <mogami@saijou-no-MacBook-Air.local>
2013-10-06 18:52:51 +0900

    Make the caret to blink

--- front/MyView.h
+++ front/MyView.h
@@ -1,21 +1,1 @@
-#import <Cocoa/Cocoa.h>
-
-@interface MyView : NSView
-{
-    NSMutableString* line;
-	int cursorOn;
-	NSTimer *timer;
-}
-- (void) drawRect : (NSRect) rect;
-- (void) awakeFromNib;
-- (void) keyDown : (NSEvent *) theEvent;
-- (void) mouseDown:(NSEvent *)theEvent;
-
-// introduced from CircleView for test
-- (IBAction)startAnimation:(id)sender;
-- (IBAction)stopAnimation:(id)sender;
-- (IBAction)toggleAnimation:(id)sender;
-
-- (void)performAnimation:(NSTimer *)aTimer;
-
-@end
+#import <Cocoa/Cocoa.h>@interface MyView : NSView{    NSMutableString* line;	int cursorOn;	NSTimer *timer;}- (void) drawRect : (NSRect) rect;- (void) awakeFromNib;- (void) keyDown : (NSEvent *) theEvent;- (void) mouseDown:(NSEvent *)theEvent;// introduced from CircleView for test- (void) startAnimation;- (void) stopAnimation;- (void) toggleAnimation;- (void)performAnimation:(NSTimer *)aTimer;@end
