b1fa27cc81e4d9cb4d53cc14e02aea7b1c253ef2
最上嗣生 <mogami@saijou-no-MacBook-Air.local>
2013-10-08 07:52:46 +0900

    change linebreak CR -> LF

--- front/MyView.h
+++ front/MyView.h
@@ -1,1 +1,17 @@
-#import <Cocoa/Cocoa.h>@interface MyView : NSView{    NSMutableString* line;	int cursorOn;	NSTimer *timer;}- (void) drawRect : (NSRect) rect;- (void) awakeFromNib;- (void) keyDown : (NSEvent *) theEvent;- (void) mouseDown:(NSEvent *)theEvent;// introduced from CircleView for test- (void) startAnimation;- (void) stopAnimation;- (void) toggleAnimation;- (void)performAnimation:(NSTimer *)aTimer;@end
+#import <Cocoa/Cocoa.h>
+@interface MyView : NSView
+{
+    NSMutableString* line;
+	int cursorOn;
+	NSTimer *timer;
+}
+- (void) drawRect : (NSRect) rect;
+- (void) awakeFromNib;
+- (void) keyDown : (NSEvent *) theEvent;
+- (void) mouseDown:(NSEvent *)theEvent;
+// introduced from CircleView for test
+- (void) startAnimation;
+- (void) stopAnimation;
+- (void) toggleAnimation;
+- (void)performAnimation:(NSTimer *)aTimer;
+@end
